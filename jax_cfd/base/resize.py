a8000db98cc709ad2ad7a49f6c83646e0b634968
JAX-CFD team <no-reply@google.com>
2021-08-17 16:23:59 -0700

    Internal change
    PiperOrigin-RevId: 391402303

--- jax_cfd/base/resize.py
+++ jax_cfd/base/resize.py
@@ -11,14 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Resize velocity fields to a different resolution grid."""
+import functools
+
+import gin
 import jax.numpy as jnp
 from jax_cfd.base import array_utils as arr_utils
 from jax_cfd.base import grids
+from google3.research.simulation.whirl.experiments.convection import nonperiodic_boundary_conditions as bc
+
+NONPERIODIC = 'dirichlet'
+PERIODIC = 'periodic'
 
 
-def downsample_staggered_velocity_component(u, direction, factor):
+def downsample_staggered_velocity_component(u, direction, factors):
   """Downsamples `u`, an array of velocities in the given `direction`.
 
   Downsampling consists of the following steps:
@@ -49,30 +55,160 @@ def downsample_staggered_velocity_component(u, direction, factor):
   Args:
     u: an array of velocity values.
     direction: an integer indicating the direction of the velocities `u`.
-    factor: the factor by which to downsample.
+    factors: the tuple of factor by which to downsamplein each direction.
+
+  Returns:
+    Coarse-grained array, reduced in size by ``factors[axis]`` along each axis.
+  """
+  if isinstance(factors, int):
+    factors = tuple(factors for _ in range(u.ndim))
+  w = arr_utils.slice_along_axis(
+      u, direction, slice(factors[direction] - 1, None, factors[direction]))
+  block_size = tuple(1 if j == direction else factors[j] for j in range(u.ndim))
+  return arr_utils.block_reduce(w, block_size, jnp.mean)
+
+
+def subsample_staggered_velocity_component(u, direction, factors):
+  """Downsamples `u`, an array of velocities in the given `direction`.
+
+  Downsampling consists of the following steps:
+    * Establish new downsampled control volumes. Each will consist of
+      `factor ** dimension` of the fine-grained control volumes.
+    * Discard all of the `u` values that do not lie on a face of the new control
+      volume in all directions.
+
+  This procedure guarantees that if our source velocity has zero divergence
+  (i.e., corresponds to an incompressible flow), the downsampled velocity field
+  also has zero divergence.
+
+  For example,
+
+  ```
+  u = [[0, 1, 2, 3],
+       [4, 5, 6, 7],
+       [8, 9, 10, 11],
+       [12, 13, 14, 15]]
+  w = subsample_velocity(u, direction=None, factor=2)
+
+  assert w == np.array([[5, 7],
+                        [9, 11]])
+  ```
+
+  Args:
+    u: an array of velocity values.
+    direction: an integer indicating the direction of the velocities `u`.
+    factors: the tuple of factor by which to downsamplein each direction.
 
   Returns:
     Coarse-grained array, reduced in size by ``factor`` along each dimension.
   """
-  w = arr_utils.slice_along_axis(u, direction, slice(factor - 1, None, factor))
-  block_size = tuple(1 if j == direction else factor for j in range(u.ndim))
-  return arr_utils.block_reduce(w, block_size, jnp.mean)
+  del direction
+  if isinstance(factors, int):
+    factors = tuple(factors for _ in range(u.ndim))
+  for direction, factor in enumerate(factors):
+    u = arr_utils.slice_along_axis(u, direction, slice(factor - 1, None,
+                                                       factor))
+  return u
 
 
-def downsample_staggered_velocity(
-    source_grid,
-    destination_grid,
-    velocity,
-):
-  """Downsamples each component of `v` by `factor`."""
-  factor = destination_grid.step[0] / source_grid.step[0]
-  assert destination_grid.domain == source_grid.domain
-  assert round(factor) == factor, factor
-  result = []
-  for j, u in enumerate(velocity):
-    if isinstance(u, grids.AlignedArray):
-      downsample = grids.applied(downsample_staggered_velocity_component)
-    else:
-      downsample = downsample_staggered_velocity_component
-    result.append(downsample(u, j, round(factor)))
-  return tuple(result)
+@gin.configurable
+def downsample_staggered_velocity(source_grid,
+                                  destination_grid,
+                                  velocity,
+                                  boundary_fn=None):
+  """Downsamples each component of `v` by `factor`.
+
+  1) This function preserves 0 divergence if the velocity field has 0
+  divergence: if the grid is reduced by the same factor in each direction,
+  this uses the old version. If the grid is reduced by different factors in
+  different directions, or if the grid has nonperiodic boundary, then a
+  different method is used: for each nonperiodic boundary, since boundary
+  location has to be preserved, e.g. if
+  source.grid.boundaries[0] == NONPERIODIC and if u is x velocity, then every
+  "factor" value of u is taken on the interior. That way the offset of the
+  wall is preserved. At the same time, v is downsampled using
+  downsample_staggered_velocity_component in 0th direction. That preserves
+  divergence.
+  2) This function allows to the input to contain as many scalars as needed.
+  Each scalar is subsampled using subsample_staggered_velocity_component.
+
+  Args:
+   source_grid: grids.Grid where velocity is located.
+   destination_grid: grids.Grid onto which to interpolate.
+   velocity: [AlighedArray,..], velocity=(x_velocity, y_velocity, [z_velocity],
+     scalar1, scalar2, ..)
+   boundary_fn: for nonperiodic conditions, boundary_fn updates ghost cells.
+
+  Returns:
+   downsampled velocity and scalar field.
+  """
+  if boundary_fn is None:
+
+    def boundary_fn(grid):  # pylint: disable=function-redefined
+      # default velocity offsets.
+      if grid.ndim == 2:
+        offsets = [(1.0, 0.5), (0.5, 1.0)]
+      elif grid.ndim == 3:
+        offsets = [(1.0, 0.5, 0.5), (0.5, 1.0, 0.5), (0.5, 0.5, 1.0)]
+
+      def _apply(v):
+        v = tuple([
+            grids.AlignedArray(u.data, offset) for u, offset in zip(v, offsets)
+        ])
+        return v
+
+      return _apply
+
+  result = [u for u in velocity]
+  factors_tuple = [
+      [1 for _ in range(source_grid.ndim)] for _ in range(source_grid.ndim)
+  ]
+  for axis in range(source_grid.ndim):
+    factor = destination_grid.step[axis] / source_grid.step[axis]
+    # uncomment when ghost cells are implemented separately from Grids.domain
+    # assert destination_grid.domain == source_grid.domain
+    assert round(factor) == factor, factor
+    factors_tuple[axis][axis] = round(factor)
+  downsample = downsample_staggered_velocity_component
+  subsample = subsample_staggered_velocity_component
+  # use downsample_staggered_velocity_component in all directions if the factors
+  # are the same in all directions.
+  if len(set([
+      factors_tuple[axis][axis] for axis in range(source_grid.ndim)
+  ])) == 1 and len(set(
+      source_grid.boundaries)) == 1 and source_grid.boundaries[0] == PERIODIC:
+    subsample = downsample_staggered_velocity_component
+  # these functions are identity if the domain is fully periodic.
+  # otherwise interior_points reduces a scalar to interior points only.
+  # offset = (0.5, 0.5) guarantees that cells 0 and -1 will be deleted.
+  # This will work for 3D periodic case as well.
+  interior_points = functools.partial(
+      bc.reduce_to_interior, grid=source_grid, offset=(0.5, 0.5))
+  # pad adds 0 valued ghost cells along nonperioidic boundaries
+  pad = functools.partial(
+      bc.interior_pad, grid=destination_grid, offset=(0.5, 0.5))
+
+  # velocity:
+  for axis, factors in enumerate(factors_tuple):
+    for j, u in enumerate(result[:source_grid.ndim]):
+      if axis == j:
+        reduction_fn = subsample
+      else:
+        reduction_fn = downsample
+
+      result[j] = pad(reduction_fn(interior_points(u), j, factors))
+
+  # scalars:
+  # currently simply takes every nth scalar where n=factor.
+  subsample = subsample_staggered_velocity_component
+  for j in range(source_grid.ndim, len(velocity)):
+    result[j] = pad(
+        subsample(
+            interior_points(result[j]), None,
+            [factors_tuple[axis][axis] for axis in range(source_grid.ndim)]))
+  # applies boundary function and converts result to AlighedArray. Default only
+  # converts to AlighedArray.
+  result = boundary_fn(destination_grid)(tuple(result))
+  if not isinstance(velocity[0], grids.AlignedArray):
+    result = tuple([u.data for u in result])
+  return result
--- jax_cfd/base/resize_test.py
+++ jax_cfd/base/resize_test.py
@@ -11,42 +11,248 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Tests for jax_cfd.base.resize."""
+
+import functools
 
 from absl.testing import absltest
 from absl.testing import parameterized
-
+from jax_cfd.base import finite_differences as fd
+from jax_cfd.base import grids
+from jax_cfd.base import interpolation
+from jax_cfd.base import pressure
 from jax_cfd.base import resize
 from jax_cfd.base import test_util
 import numpy as np
+
+from google3.research.simulation.whirl.experiments.convection import implicit_solvers
+from google3.research.simulation.whirl.experiments.convection import nonperiodic_boundary_conditions as bc
+
+SIZE_X = 8
 
 
 class ResizeTest(test_util.TestCase):
 
   @parameterized.parameters(
-      dict(u=np.array([[0, 1, 2, 3],
-                       [4, 5, 6, 7],
-                       [8, 9, 10, 11],
-                       [12, 13, 14, 15]]),
-           direction=0,
-           factor=2,
-           expected=np.array([[4.5, 6.5],
-                              [12.5, 14.5]])),
-      dict(u=np.array([[0, 1, 2, 3],
-                       [4, 5, 6, 7],
-                       [8, 9, 10, 11],
-                       [12, 13, 14, 15]]),
-           direction=1,
-           factor=2,
-           expected=np.array([[3., 5.],
-                              [11., 13.]])),
+      dict(
+          u=np.array([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11],
+                      [12, 13, 14, 15]]),
+          direction=0,
+          factor=2,
+          expected=np.array([[4.5, 6.5], [12.5, 14.5]])),
+      dict(
+          u=np.array([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11],
+                      [12, 13, 14, 15]]),
+          direction=1,
+          factor=2,
+          expected=np.array([[3., 5.], [11., 13.]])),
   )
   def testDownsampleVelocityComponent(self, u, direction, factor, expected):
     """Test `downsample_array` produces the expected results."""
     actual = resize.downsample_staggered_velocity_component(
         u, direction, factor)
     self.assertAllClose(expected, actual, atol=1e-6)
+
+  @parameterized.parameters(
+      dict(
+          u=np.array([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11],
+                      [12, 13, 14, 15]]),
+          factor=2,
+          expected=np.array([[5, 7], [13, 15]])),
+  )
+  def testSubsampleVelocityComponent(self, u, factor, expected):
+    """Test `downsample_array` produces the expected results."""
+    actual = resize.subsample_staggered_velocity_component(
+        u, None, factor)
+    self.assertAllClose(expected, actual, atol=1e-6)
+
+  @parameterized.parameters(
+      ((1, 2),),
+      ((2, 1),),
+      ((2, 2),),
+      ((1, 1),),
+  )
+  def test_downsample_staggered_velocity_periodic_dirichlet(self, factors):
+
+    def default_boundary_fn_velocity(grid):
+      velocity_bc = functools.partial(bc.non_slip_bc_vector, grid=grid)
+      offsets = [(1.0, 0.5), (0.5, 1.0)]
+
+      def _apply(v):
+        v = tuple([
+            grids.AlignedArray(u.data, offset) for u, offset in zip(v, offsets)
+        ])
+        v = velocity_bc(v)
+        return v
+
+      return _apply
+
+    rs = np.random.RandomState(0)
+    grid = grids.Grid(
+        (SIZE_X, SIZE_X + 2),
+        domain=((0, 1), (-1 / float(SIZE_X), 1 + 1 / float(SIZE_X))),
+        boundaries=('periodic', 'dirichlet'))
+    b = grids.AlignedArray(
+        rs.randn(SIZE_X, SIZE_X + 2).astype(np.float32), (0.5, 1.0))
+    b1 = grids.AlignedArray(
+        rs.randn(SIZE_X, SIZE_X + 2).astype(np.float32), (1.0, 0.5))
+    velocity = (b1, b)
+    velocity = bc.non_slip_bc_vector(velocity, grid)
+    pressure_solve = implicit_solvers.pressure_solve_fast_diag(
+        grid, boundary_type=(('periodic',), ('neumann',)))
+    velocity = pressure.projection(velocity, grid, pressure_solve)
+    velocity = bc.non_slip_bc_vector(velocity, grid)
+
+    destination_grid = grids.Grid(
+        (SIZE_X // factors[0], SIZE_X // factors[1] + 2),
+        domain=((0, 1), (-1 / float(SIZE_X) * factors[1],
+                         1 + 1 / float(SIZE_X) * factors[1])),
+        boundaries=('periodic', 'dirichlet'))
+    velocity_reduced = resize.downsample_staggered_velocity(
+        grid, destination_grid, velocity, default_boundary_fn_velocity)
+    # checks that zero divergence is preserved
+    self.assertAllClose(
+        bc.reduce_to_interior(
+            fd.divergence(velocity_reduced, destination_grid),
+            destination_grid),
+        0,
+        atol=1e-5)
+    x_velocity = velocity_reduced[0]
+    y_velocity = velocity_reduced[1]
+    # checks that the final size of the reduced velocity equals destination_grid
+    self.assertEqual(x_velocity.data.shape,
+                     (SIZE_X // factors[0], SIZE_X // factors[1] + 2))
+    self.assertEqual(y_velocity.data.shape,
+                     (SIZE_X // factors[0], SIZE_X // factors[1] + 2))
+    # checks that the dirichlet boundary is satisfied
+    self.assertAllClose(x_velocity.data[:, 0] + x_velocity.data[:, 1], 0)
+    self.assertAllClose(x_velocity.data[:, -1] + x_velocity.data[:, -2], 0)
+    self.assertAllClose(y_velocity.data[:, 0], 0)
+    self.assertAllClose(y_velocity.data[:, -2], 0)
+
+    # checks that it does nothing if no downsampling
+    if factors[0] == factors[1] == 1:
+      self.assertAllClose(x_velocity.data, velocity[0].data)
+      self.assertAllClose(y_velocity.data, velocity[1].data)
+
+  @parameterized.parameters(
+      ((1, 2),),
+      ((2, 1),),
+      ((2, 2),),
+      ((1, 1),),
+  )
+  def test_downsample_staggered_velocity_periodic_periodic(self, factors):
+    rs = np.random.RandomState(0)
+    grid = grids.Grid((SIZE_X, SIZE_X),
+                      domain=((0, 1), (0, 1)),
+                      boundaries=('periodic', 'periodic'))
+    b = grids.AlignedArray(
+        rs.randn(SIZE_X, SIZE_X).astype(np.float32), (0.5, 1.0))
+    b1 = grids.AlignedArray(
+        rs.randn(SIZE_X, SIZE_X).astype(np.float32), (1.0, 0.5))
+    velocity = (b1, b)
+    velocity = bc.non_slip_bc_vector(velocity, grid)
+    pressure_solve = implicit_solvers.pressure_solve_fast_diag(
+        grid, boundary_type=(('periodic',), ('periodic',)))
+    velocity = pressure.projection(velocity, grid, pressure_solve)
+    velocity = bc.non_slip_bc_vector(velocity, grid)
+
+    destination_grid = grids.Grid((SIZE_X // factors[0], SIZE_X // factors[1]),
+                                  domain=((0, 1), (0, 1)),
+                                  boundaries=('periodic', 'periodic'))
+    velocity_reduced = resize.downsample_staggered_velocity(
+        grid, destination_grid, velocity)
+    # checks that zero divergence is preserved
+    self.assertAllClose(
+        bc.reduce_to_interior(
+            fd.divergence(velocity_reduced, destination_grid),
+            destination_grid),
+        0,
+        atol=1e-5)
+    x_velocity = velocity_reduced[0]
+    y_velocity = velocity_reduced[1]
+    # checks that the final size of the reduced velocity equals destination_grid
+    self.assertEqual(x_velocity.data.shape,
+                     (SIZE_X // factors[0], SIZE_X // factors[1]))
+    self.assertEqual(y_velocity.data.shape,
+                     (SIZE_X // factors[0], SIZE_X // factors[1]))
+
+    # checks that it does nothing if no downsampling
+    if factors[0] == factors[1] == 1:
+      self.assertAllClose(x_velocity.data, velocity[0].data)
+      self.assertAllClose(y_velocity.data, velocity[1].data)
+
+  @parameterized.parameters(
+      ((1, 2), (1.0, 0.5)),
+      ((2, 1), (1.0, 0.5)),
+      ((2, 2), (1.0, 0.5)),
+      ((1, 1), (1.0, 0.5)),
+      ((1, 2), (0.5, 1.0)),
+      ((2, 1), (0.5, 1.0)),
+      ((2, 2), (0.5, 1.0)),
+      ((1, 1), (0.5, 1.0)),
+  )
+  def test_downsample_staggered_velocity_periodic_dirichlet_scalar(
+      self, factors, scalar_offset):
+
+    def default_boundary_fn(grid):
+      velocity_bc = functools.partial(bc.non_slip_bc_vector, grid=grid)
+      temperture_bc = functools.partial(
+          bc.dirichlet_bc_scalar, grid=grid, value_hi=0.0, value_lo=1.0)
+      offsets = [(1.0, 0.5), (0.5, 1.0), scalar_offset]
+
+      def _apply(v_c):
+        v_c = tuple([
+            grids.AlignedArray(u.data, offset)
+            for u, offset in zip(v_c, offsets)
+        ])
+        v = v_c[:2]
+        c = v_c[-1]
+        v = velocity_bc(v)
+        return (v[0], v[1], temperture_bc(c))
+
+      return _apply
+
+    rs = np.random.RandomState(0)
+    grid = grids.Grid(
+        (SIZE_X, SIZE_X + 2),
+        domain=((0, 1), (-1 / float(SIZE_X), 1 + 1 / float(SIZE_X))),
+        boundaries=('periodic', 'dirichlet'))
+    b = grids.AlignedArray(
+        rs.randn(SIZE_X, SIZE_X + 2).astype(np.float32), (0.5, 1.0))
+    b1 = grids.AlignedArray(
+        rs.randn(SIZE_X, SIZE_X + 2).astype(np.float32), (1.0, 0.5))
+    scalar = grids.AlignedArray(
+        rs.randn(SIZE_X, SIZE_X + 2).astype(np.float32), scalar_offset)
+    velocity_scalar = (b1, b, scalar)
+    velocity_scalar = default_boundary_fn(grid)(velocity_scalar)
+    destination_grid = grids.Grid(
+        (SIZE_X // factors[0], SIZE_X // factors[1] + 2),
+        domain=((0, 1), (-1 / float(SIZE_X) * factors[1],
+                         1 + 1 / float(SIZE_X) * factors[1])),
+        boundaries=('periodic', 'dirichlet'))
+    velocity_scalar_reduced = resize.downsample_staggered_velocity(
+        grid, destination_grid, velocity_scalar, default_boundary_fn)
+    x_velocity = velocity_scalar_reduced[0]
+    y_velocity = velocity_scalar_reduced[1]
+    scalar = velocity_scalar_reduced[2]
+    # checks that the final size of the reduced velocity equals destination_grid
+    self.assertEqual(x_velocity.data.shape,
+                     (SIZE_X // factors[0], SIZE_X // factors[1] + 2))
+    self.assertEqual(y_velocity.data.shape,
+                     (SIZE_X // factors[0], SIZE_X // factors[1] + 2))
+    # checks that the final size of the reduced scalar equals destination_grid
+    self.assertEqual(scalar.data.shape,
+                     (SIZE_X // factors[0], SIZE_X // factors[1] + 2))
+    # checks that it does nothing if no downsampling
+    if factors[0] == factors[1] == 1:
+      self.assertAllClose(scalar.data, velocity_scalar[2].data)
+
+    # checks that the dirichlet boundary is satisfied
+    scalar_interp = interpolation.linear(
+        scalar, offset=(0.5, 1.0), grid=destination_grid)
+    self.assertAllClose(scalar_interp.data[:, 0], 1.0, atol=1e-6)
+    self.assertAllClose(scalar_interp.data[:, -2], 0, atol=1e-6)
 
 
 if __name__ == '__main__':

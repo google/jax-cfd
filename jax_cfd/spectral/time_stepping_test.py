4f587d886d9fa199f5ca4d35b79f92bcf5eb69de
Sergei Lebedev <slebedev@google.com>
2023-10-18 08:45:15 -0700

    Updated jax.config import
    PiperOrigin-RevId: 574488049

--- jax_cfd/spectral/time_stepping_test.py
+++ jax_cfd/spectral/time_stepping_test.py
@@ -18,7 +18,7 @@ from absl.testing import absltest
 from absl.testing import parameterized
 import jax
 from jax import tree_util
-from jax.config import config
+from jax import config
 import jax.numpy as jnp
 from jax_cfd.base import funcutils
 from jax_cfd.spectral import time_stepping
